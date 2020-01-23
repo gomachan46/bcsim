1e38df1c2e2b03a43d3a847c410b99527f79fb32
gomachan46 <shiro.gomachan46@gmail.com>
2020-01-24 04:21:42 +0900

    add card icon example

---
+++ components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="~/assets/images/bc_icon.jpg" alt="bc icon" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">bcsim</div>
+      <p class="text-gray-700 text-base">
+        Blood Card Simulator.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
