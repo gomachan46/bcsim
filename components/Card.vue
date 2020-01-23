8deab146db859fa573ffde287c53ae3a215402f9
gomachan46 <shiro.gomachan46@gmail.com>
2020-01-24 04:25:30 +0900

    fix card text

--- components/Card.vue
+++ components/Card.vue
@@ -4,7 +4,10 @@
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">bcsim</div>
       <p class="text-gray-700 text-base">
-        Blood Card Simulator.
+        Blood Card Simulator
+      </p>
+      <p class="text-gray-700 text-base">
+        (This is an unofficial tool)
       </p>
     </div>
   </div>
